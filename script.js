877b76b39b85e364993ea5ccf2a8786cc2f82ce0
SAMMBENDE <51370083+SAMMBENDE@users.noreply.github.com>
2022-07-12 06:12:58 +0200

    changed typing

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Learner"],
+        strings: ["Fullstack", "Developer", "Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
